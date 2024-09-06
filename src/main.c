90dd549a7a2f090e2b2517f84709d0578beb1074
Ryan Hutchison <rwhutchison@live.com>
2024-09-06 18:09:44 -0500

    Use bools to represent success/failure of funcs

--- src/main.c
+++ src/main.c
@@ -1,23 +1,38 @@
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "vendors/GLFW/glfw3.h"
 
 
 GLFWwindow* window;
 
-int present_window(void) {
-  /* Initialize the library */
+bool configure_glfw(void) {
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
   if (!glfwInit()) {
     fprintf(stderr, "Unable to Initialize glfw library\n");
-    return EXIT_FAILURE;
+    return false;
+  }
+  
+  return true;
+}
+
+bool present_window(void) {
+
+  if(!configure_glfw()) {
+    fprintf(stderr, "Unable to configure glfw library\n");
+    return false;
   }
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
   if (!window) {
       glfwTerminate();
-      return EXIT_FAILURE;
+      return false;
   }
 
   /* Make the window's context current */
@@ -36,7 +51,7 @@ int present_window(void) {
   }
 
   glfwTerminate();
-  return EXIT_SUCCESS;
+  return true;
 }
 
 int main(void) {
