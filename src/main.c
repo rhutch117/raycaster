a69352920811a69ddcf97ff18a99b079ae0c5a39
Ryan Hutchison <rwhutchison@live.com>
2024-09-07 12:09:43 -0500

    Complete setup and present a window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 GLFWwindow* window;
+
+void processInput(GLFWwindow *window) {
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// Callback function being called everytime the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
 bool configure_glfw(void) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -28,23 +42,40 @@ bool present_window(void) {
     return false;
   }
 
-  window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+  window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL", NULL, NULL);
   if (!window) {
+      fprintf(stderr, "Unable to create glfw window\n");
       glfwTerminate();
       return false;
   }
 
   glfwMakeContextCurrent(window);
 
+  // GLAD Initializes function pointers for OpenGL so we must
+  // initialize GLAD before we call any OpenGL function.
+  // GLFW returns the function addresses (which is OS specific),
+  // and then these addresses are loaded into GLAD.
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      fprintf(stderr, "Unable to create glfw window\n");
+      return false;
+  }
+
+  // Create our viewport and register our resize callback function
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
   while (!glfwWindowShouldClose(window)) {
-      /* Render here */
-      glClear(GL_COLOR_BUFFER_BIT);
 
-      /* Swap front and back buffers */
-      glfwSwapBuffers(window);
+    // Handle any user input
+    processInput(window);
 
-      /* Poll for and process events */
-      glfwPollEvents();
+    /* Render here */
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT); // clear the screen to avoid seeing the previous frame
+
+    // Check and call events and swap the buffers
+    glfwPollEvents();
+    glfwSwapBuffers(window);
   }
 
   glfwTerminate();
